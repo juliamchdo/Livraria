9f434f076fbc6780ac5251d6e125128bf13a37df
juliamchdo <89415625+juliamchdo@users.noreply.github.com>
2021-09-12 16:51:58 -0300

    Add files via upload

---
+++ Livraria/Livraria/Form1.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MySql.Data.MySqlClient;
+
+namespace Livraria
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private MySqlConnectionStringBuilder conexaoBanco()
+        {
+            MySqlConnectionStringBuilder conexaoBD = new MySqlConnectionStringBuilder();
+            conexaoBD.Server = "localhost";
+            conexaoBD.Database = "livraria";
+            conexaoBD.UserID = "root";
+            conexaoBD.Password = "";
+            conexaoBD.SslMode = 0;
+            return conexaoBD;
+        }
+
+        private void LimparCampos()
+        {
+            tbNome.Clear();
+            tbGenero.Clear();
+            tbDescricao.Clear();
+            tbAno.Clear();
+        }
+        private void btLimpar_Click(object sender, EventArgs e)
+        {
+            LimparCampos();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            atualizarDataGrid();
+        }
+
+        private void atualizarDataGrid()
+        {
+            MySqlConnectionStringBuilder conexaoBD = conexaoBanco();
+            MySqlConnection realizaConexacoBD = new MySqlConnection(conexaoBD.ToString());
+
+            try
+            {
+                realizaConexacoBD.Open();
+
+                MySqlCommand comandoMySql = realizaConexacoBD.CreateCommand();
+                comandoMySql.CommandText = "SELECT * FROM livros WHERE ativoLivro = 1";
+                MySqlDataReader reader = comandoMySql.ExecuteReader();
+
+                dgLivraria.Rows.Clear();
+
+                while (reader.Read())
+                {
+                    DataGridViewRow row = (DataGridViewRow)dgLivraria.Rows[0].Clone();//FAZ UM CAST E CLONA A LINHA DA TABELA
+                    row.Cells[0].Value = reader.GetInt32(0);//ID
+                    row.Cells[1].Value = reader.GetString(1);//NOME
+                    row.Cells[2].Value = reader.GetString(2);//GENERO
+                    row.Cells[3].Value = reader.GetString(3);//DESCRICAO
+                    row.Cells[4].Value = reader.GetString(4);//ANO
+                    dgLivraria.Rows.Add(row);//ADICIONO A LINHA NA TABELA
+                }
+
+                realizaConexacoBD.Close();
+            }
+
+            catch (Exception ex)
+            {
+                MessageBox.Show("Não foi possível executar a ação.");
+                Console.WriteLine(ex.Message);
+            }
+        }
+
+        private void btInserir_Click(object sender, EventArgs e)
+        {
+
+            MySqlConnectionStringBuilder conexaoBD = conexaoBanco();
+            MySqlConnection realizaConexacoBD = new MySqlConnection(conexaoBD.ToString());
+            try
+            {
+                realizaConexacoBD.Open();
+
+                MySqlCommand comandoMySql = realizaConexacoBD.CreateCommand();
+
+                comandoMySql.CommandText = "INSERT INTO livros (nomelivro,generoLivro,descricaoLivro,anoLivro) " +
+                    "VALUES('" + tbNome.Text + "', '" + tbGenero.Text + "','" + tbDescricao.Text + "', " + Convert.ToInt16(tbAno.Text) + ")";
+                comandoMySql.ExecuteNonQuery();
+
+                realizaConexacoBD.Close();
+                MessageBox.Show("Livro inserido com sucesso!");
+
+                atualizarDataGrid();
+                LimparCampos();
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
